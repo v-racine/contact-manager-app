4a814e6f17865a42b36758d756880ca41255adf6
Valerie Racine <vracine21@gmail.com>
2025-10-07 14:37:01 -0700

    cleanup refactor

--- public/javascripts/fetchers.js
+++ public/javascripts/fetchers.js
@@ -10,23 +10,6 @@ const fetchData = async (path) => {
   }
   return await response.json();
 };
-// const fetchData = async (path) => {
-//   try {
-//     const url = new URL(baseURL + path);
-
-//     const response = await fetch(url);
-//     if (!response.ok) {
-//       throw new Error(`HTTP error! Status: ${response.status}`);
-//     }
-//     message.textContent = "";
-//     const data = await response.json();
-//     return data;
-//   } catch (error) {
-//     message.textContent = GENERIC_ERROR_MESSAGE;
-//     console.error("Could not fetch data:", error);
-//     return null;
-//   }
-// };
 
 //POST fetch request to add a contact
 const postNewData = async (path, data) => {
@@ -50,32 +33,7 @@ const postNewData = async (path, data) => {
   }
 };
 
-// const postNewData = async (path, data) => {
-//   try {
-//     const url = new URL(baseURL + path);
-//     const submission = await fetch(url, {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json; charset=utf-8",
-//       },
-//       body: data,
-//     });
-
-//     if (submission.status === 201) {
-//       message.textContent = "";
-//       const responseData = await submission.json();
-//       console.log(
-//         `This contact was sucessfully added: ${JSON.stringify(responseData)}`
-//       );
-//       return responseData;
-//     } else {
-//       throw new Error(`HTTP request error: ${submission.status}`);
-//     }
-//   } catch (err) {
-//     message.textContent = GENERIC_ERROR_MESSAGE;
-//     console.error("Could not send data:", err);
-//     return null;
-//   }
-// };
-
 //PUT fetch request to edit existing contact
+const editContactData = async (path, data) => {
+  //STUB
+};
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -7,26 +7,7 @@ const resultsWrapper = document.querySelector(".results");
 const contactContainer = document.querySelector(".contact-container");
 const message = document.querySelector("#message-for-user");
 
-// //GET fetch for contacts
-// const fetchData = async (path) => {
-//   try {
-//     const url = new URL(baseURL + path);
-
-//     const response = await fetch(url);
-//     if (!response.ok) {
-//       throw new Error(`HTTP error! Status: ${response.status}`);
-//     }
-//     message.textContent = "";
-//     const data = await response.json();
-//     return data;
-//   } catch (error) {
-//     message.textContent = GENERIC_ERROR_MESSAGE;
-//     console.error("Could not fetch data:", error);
-//     return null;
-//   }
-// };
-
-// contacts search
+// Search existing contacts
 const onInput = async (e) => {
   const searchTerm = e.target.value.toLowerCase();
   try {
@@ -38,11 +19,11 @@ const onInput = async (e) => {
       return contact.full_name.toLowerCase().includes(searchTerm);
     });
 
-    if (!filteredContacts.length || searchTerm) {
+    if (!filteredContacts.length || !searchTerm) {
       dropdown.classList.remove("is-active");
+    } else {
+      dropdown.classList.add("is-active");
     }
-
-    dropdown.classList.add("is-active");
 
     for (let contact of filteredContacts) {
       const option = document.createElement("a");
@@ -66,32 +47,16 @@ const onInput = async (e) => {
 
 searchInputEle.addEventListener("input", debounce(onInput));
 
-//click out of search's dropdown menu
+//Click out of search's dropdown menu
 document.addEventListener("click", (e) => {
   if (!searchInputEle.contains(e.target)) {
     dropdown.classList.remove("is-active");
+    searchInputEle.value = "";
   }
 });
 
-//all contacts display
+//Display all contacts
 const allContactsBtn = document.querySelector("#all-contacts");
-
-// const onAllContacts = async () => {
-//   const contacts = await fetchData("/api/contacts");
-//   if (!contacts) return;
-
-//   form.style.display = "none";
-//   message.textContent = "";
-//   contactContainer.textContent = "";
-//   const fragment = document.createDocumentFragment();
-
-//   for (let contact of contacts) {
-//     const contactDisplay = contactTemplate(contact);
-//     fragment.appendChild(contactDisplay);
-//   }
-
-//   contactContainer.appendChild(fragment);
-// };
 
 const onAllContacts = async () => {
   try {
@@ -115,18 +80,7 @@ const onAllContacts = async () => {
 
 allContactsBtn.addEventListener("click", onAllContacts);
 
-//single contact selection
-// const onContactSelect = async (contactId) => {
-//   const contactData = await fetchData(`/api/contacts/${contactId}`);
-
-//   if (!contactData) return;
-
-//   form.style.display = "none";
-//   message.textContent = "";
-//   contactContainer.textContent = "";
-//   contactContainer.appendChild(contactTemplate(contactData));
-// };
-
+//Select and display single contact
 const onContactSelect = async (contactId) => {
   try {
     const contactData = await fetchData(`/api/contacts/${contactId}`);
@@ -143,41 +97,7 @@ const onContactSelect = async (contactId) => {
   }
 };
 
-// const contactTemplate = (contactData) => {
-//   const article = document.createElement("article");
-//   article.className = "contact";
-
-//   const mediaContent = document.createElement("div");
-//   mediaContent.className = "contact-content";
-
-//   const content = document.createElement("div");
-//   content.className = "content";
-
-//   const h4 = document.createElement("h2");
-//   h4.textContent = contactData.full_name;
-
-//   const pEmail = document.createElement("p");
-//   pEmail.textContent = contactData.email;
-
-//   const pPhone = document.createElement("p");
-//   pPhone.textContent = `Phone #: ${contactData.phone_number}`;
-
-//   const editBtn = document.createElement("button");
-//   editBtn.className = "button is-info is-light is-small is-rounded";
-//   editBtn.textContent = "Edit";
-
-//   const deleteBtn = document.createElement("button");
-//   deleteBtn.className = "button is-danger is-light is-small is-rounded";
-//   deleteBtn.textContent = "Delete";
-
-//   content.append(h4, pEmail, pPhone, editBtn, deleteBtn);
-//   mediaContent.appendChild(content);
-//   article.appendChild(mediaContent);
-
-//   return article;
-// };
-
-//Add new contact display
+//Display "new contact" form
 const addContactBtn = document.querySelector("#add-contact");
 const form = document.querySelector("form");
 
@@ -186,32 +106,3 @@ addContactBtn.addEventListener("click", () => {
   contactContainer.textContent = "";
   form.style.display = "block";
 });
-
-// //POST fetch request to add a contact
-// const postNewData = async (path, data) => {
-//   try {
-//     const url = new URL(baseURL + path);
-//     const submission = await fetch(url, {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json; charset=utf-8",
-//       },
-//       body: data,
-//     });
-
-//     if (submission.status === 201) {
-//       message.textContent = "";
-//       const responseData = await submission.json();
-//       console.log(
-//         `This contact was sucessfully added: ${JSON.stringify(responseData)}`
-//       );
-//       return responseData;
-//     } else {
-//       throw new Error(`HTTP request error: ${submission.status}`);
-//     }
-//   } catch (err) {
-//     message.textContent = GENERIC_ERROR_MESSAGE;
-//     console.error("Could not send data:", err);
-//     return null;
-//   }
-// };
