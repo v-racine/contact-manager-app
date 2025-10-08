8f7d978f9075b87b040f3d727672316b1a693873
Valerie Racine <vracine21@gmail.com>
2025-10-08 10:16:18 -0700

    fix initController

--- public/controllers/contactController.js
+++ public/controllers/contactController.js
@@ -1,18 +1,15 @@
 import { fetchData } from "../api/contactsApi.js";
 import { renderAllContacts } from "../views/contactListView.js";
 
-export function initController() {
+export async function initController() {
   const message = document.querySelector("#message-for-user");
-  const allContactsBtn = document.querySelector("#all-contacts");
 
-  allContactsBtn.addEventListener("click", async () => {
-    try {
-      const contacts = await fetchData("/api/contacts");
-      renderAllContacts(contacts);
-    } catch (err) {
-      console.error("Error loading contacts:", err);
-      message.textContent =
-        "Sorry, something went wrong. Please try again later.";
-    }
-  });
+  try {
+    const contacts = await fetchData("/api/contacts");
+    renderAllContacts(contacts);
+  } catch (err) {
+    console.error("Error loading contacts:", err);
+    message.textContent =
+      "Sorry, something went wrong. Please try again later.";
+  }
 }
