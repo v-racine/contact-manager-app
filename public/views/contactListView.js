6b667f7646fcf14dd49ee5cda297083aa16f139c
Valerie Racine <vracine21@gmail.com>
2025-10-07 22:16:38 -0700

    feat: huge refactor part 1

---
+++ public/views/contactListView.js
@@ -0,0 +1,56 @@
+const GENERIC_ERROR_MESSAGE =
+  "Sorry, something went wrong. Please try again later.";
+
+const searchInputEle = document.querySelector("#search-input");
+const dropdown = document.querySelector(".dropdown");
+const resultsWrapper = document.querySelector(".results");
+const contactContainer = document.querySelector(".contact-container");
+const message = document.querySelector("#message-for-user");
+const allContactsBtn = document.querySelector("#all-contacts");
+const form = document.querySelector("form");
+
+export const renderAllContacts = async (contacts) => {
+  form.style.display = "none";
+  message.textContent = "";
+  contactContainer.textContent = "";
+
+  const fragment = document.createDocumentFragment();
+  contacts.forEach((contact) => {
+    fragment.appendChild(contactTemplate(contact));
+  });
+  contactContainer.appendChild(fragment);
+};
+
+const contactTemplate = (contactData) => {
+  const article = document.createElement("article");
+  article.className = "contact";
+
+  const mediaContent = document.createElement("div");
+  mediaContent.className = "contact-content";
+
+  const content = document.createElement("div");
+  content.className = "content";
+
+  const h4 = document.createElement("h2");
+  h4.textContent = contactData.full_name;
+
+  const pEmail = document.createElement("p");
+  pEmail.textContent = contactData.email;
+
+  const pPhone = document.createElement("p");
+  pPhone.textContent = `Phone #: ${contactData.phone_number}`;
+
+  const editBtn = document.createElement("button");
+  editBtn.className = "button is-info is-light is-small is-rounded";
+  editBtn.textContent = "Edit";
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.className = "button is-danger is-light is-small is-rounded";
+  deleteBtn.textContent = "Delete";
+
+  content.append(h4, pEmail, pPhone, editBtn, deleteBtn);
+  mediaContent.appendChild(content);
+  article.appendChild(mediaContent);
+
+  return article;
+};
